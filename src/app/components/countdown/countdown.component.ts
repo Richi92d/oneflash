afd04eb7e828e1409a1029fdbc87ba8b2c7efa88
VINODEA Richard <richard.vinodeaa@gmail.com>
2021-02-19 10:41:20 +0100

    add methods & click event

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.css']
 })
-export class CountdownComponent implements OnInit {
+export class CountdownComponent {
 
-  constructor() { }
+  interval: any;
+  time = new Date(0);
 
-  ngOnInit(): void {
+  startTimer() {
+    this.interval = setInterval(() => {
+      this.time.setSeconds(this.time.getSeconds() + 1);
+    }, 1000);
   }
 
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  resetTimer() {
+    this.time.setSeconds(0);
+  }
 }
