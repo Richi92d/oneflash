94e2d18c717cf25e215581a46b40695e349a07bf
VINODEA Richard <richard.vinodeaa@gmail.com>
2021-02-19 10:11:51 +0100

    add firebase config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import firebase from 'firebase';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
-  
+
+  constructor() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCKcaP14A4QktQtYjjLpTLGUSfwlg_Gc1A",
+      authDomain: "authentication-23ca2.firebaseapp.com",
+      projectId: "authentication-23ca2",
+      storageBucket: "authentication-23ca2.appspot.com",
+      messagingSenderId: "196451137310",
+      appId: "1:196451137310:web:1eab9076bffdde91ee7b0f"
+    };
+    firebase.initializeApp(firebaseConfig);
+  }
+
 }
